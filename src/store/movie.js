a01d14d6956bd72d399b001f8d4b9ee6ac9a200f
perkid <perkid90@gmail.com>
2023-10-20 03:25:03 +0900

    Add Vercel Serverless Functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -19,14 +19,13 @@ export const searchMovies = async page => {
     store.state.message = ''
   }
   try {
-    const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`)
-    // const res = await fetch('/api/movie', {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     title: store.state.searchText,
-    //     page
-    //   })
-    // })
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page
+      })
+    })
     const { Response, Search, totalResults, Error } = await res.json()
     if (Response === 'True') {
       store.state.movies = [
@@ -46,7 +45,12 @@ export const searchMovies = async page => {
 }
 export const getMovieDetails = async id => {
   try {
-    const res = await fetch(`https://omdbapi.com?apikey=7035c60c&i=${id}&plot=full`)
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        id
+      })
+    })
     store.state.movie = await res.json()
   } catch (error) {
     console.log('getMovieDetails', error)
